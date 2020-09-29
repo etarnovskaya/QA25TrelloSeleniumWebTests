f3c3d1a1bb4744a5f0066b5656f14ba0351b794d
Lena <etarnovskaya@gmail.com>
2020-09-29 21:08:44 +0300

    added toString at Board.class

--- src/test/java/com/telRan/tests/model/Board.java
+++ src/test/java/com/telRan/tests/model/Board.java
@@ -21,4 +21,12 @@ public class Board {
     public String getBoardVisibility() {
         return boardVisibility;
     }
+
+    @Override
+    public String toString() {
+        return "Board{" +
+                "boardName='" + boardName + '\'' +
+                ", boardVisibility='" + boardVisibility + '\'' +
+                '}';
+    }
 }
